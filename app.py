f954f09cb862158dc24299128adeb923d8e9ac21
qadan96 <bq55@cornell.edu>
2024-02-08 19:28:14 -0500

    changes

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
-from flask import *
+from flask import Flask, send_from_directory
 from whitenoise import WhiteNoise
 
 app = Flask(__name__)
 app.wsgi_app = WhiteNoise(app.wsgi_app, 
                           root='static/', 
-                          prefix='static/', 
-                          index_file='index.htm', 
-                          autorefresh=True)
+                          prefix='static/',
+                          index_file='index.htm',
+                          autorefresh=True)  
+
 @app.route('/', methods=['GET'])
 def index():
-    return make_response(index.htm)
-    
+    return send_from_directory('static', 'index.htm')  
+
 if __name__ == "__main__":
     app.run(threaded=True, port=5000)
